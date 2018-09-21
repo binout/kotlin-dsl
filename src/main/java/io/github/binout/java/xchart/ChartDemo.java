091d3e2523057335637f774158eb4195746184bc
Benoit Prioux <benoit.prioux@gmail.com>
2018-09-21 16:15:46 +0200

    add chart dsl

---
+++ src/main/java/io/github/binout/java/xchart/ChartDemo.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2016 Benoît Prioux
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.github.binout.java.xchart;
+
+import org.knowm.xchart.SwingWrapper;
+import org.knowm.xchart.XYChart;
+import org.knowm.xchart.XYChartBuilder;
+import org.knowm.xchart.XYSeries;
+import org.knowm.xchart.style.MatlabTheme;
+import org.knowm.xchart.style.Styler;
+
+public class ChartDemo {
+
+    public static void main(String[] args) {
+        final XYChart chart = new XYChartBuilder()
+                .width(600)
+                .height(400)
+                .title("Area Chart")
+                .xAxisTitle("X")
+                .yAxisTitle("Y").build();
+
+// Customize Chart
+        chart.getStyler().setLegendPosition(Styler.LegendPosition.InsideNE);
+        chart.getStyler().setDefaultSeriesRenderStyle(XYSeries.XYSeriesRenderStyle.Area);
+        chart.getStyler().setTheme(new MatlabTheme());
+// Series
+        chart.addSeries("a", new double[] { 0, 3, 5, 7, 9 }, new double[] { -3, 5, 9, 6, 5 });
+        chart.addSeries("b", new double[] { 0, 2, 4, 6, 9 }, new double[] { -1, 6, 4, 0, 4 });
+        chart.addSeries("c", new double[] { 0, 1, 3, 8, 9 }, new double[] { -2, -1, 1, 0, 1 });
+
+        new SwingWrapper<>(chart).displayChart();
+
+    }
+}
